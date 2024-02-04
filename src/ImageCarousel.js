3a4e05a930a17b3c044fb6ff6665ed3662a296b8
NikhilMeruva959 <43816295+NikhilMeruva959@users.noreply.github.com>
2024-02-03 21:59:14 -0500

    1 file modified

--- src/ImageCarousel.js
+++ src/ImageCarousel.js
@@ -1,65 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
 
-// let startingIndex = 1;
 export default function ImageCarousel(props) {
-  const [startingIndex, setStartingIndex] = useState(null);
+  const [startingIndex, setStartingIndex] = useState(props?.index ?? '');
   const [currImg, setCurrImg] = useState(null);
 
   useEffect(() => {
     setCurrImg(props.images[startingIndex]);
-  }, [props, startingIndex]);
-  useEffect(() => {
-    setStartingIndex(props.index);
-  }, [props.images]);
+  }, [props.images, startingIndex]);
 
   function goBack() {
     if (currImg === undefined) {
       console.log('Need to input Dog Breed');
     } else if (startingIndex === 0) {
-      setStartingIndex(props.images.length - 1);
-      setCurrImg(currImg[startingIndex]);
+      setStartingIndex((prevIndex) => props.images.length - 1);
     } else {
-      setStartingIndex(startingIndex - 1);
-      setCurrImg(currImg[startingIndex]);
+      setStartingIndex((prevIndex) => prevIndex - 1);
     }
-    console.log(startingIndex);
-    // setCurrImg(currImg[startingIndex]);
   }
-  function goFoward() {
+
+  function goForward() {
     if (currImg === undefined) {
       console.log('Need to input Dog Breed');
     } else if (startingIndex === props.images.length - 1) {
-      setStartingIndex(0);
-      setCurrImg(currImg[startingIndex]);
+      setStartingIndex((prevIndex) => 0);
     } else {
-      setStartingIndex(startingIndex + 1);
-      setCurrImg(currImg[startingIndex]);
+      setStartingIndex((prevIndex) => prevIndex + 1);
     }
-    console.log(startingIndex);
   }
-
-  // function goBack() {
-  //   if (startingIndex === 0) startingIndex = props.images.length - 1;
-  //   else startingIndex = startingIndex - 1;
-  //   console.log(startingIndex);
-  //   setCurrImg(props.images[startingIndex]);
-  // }
-  // function goFoward() {
-  //   if (startingIndex === props.images.length - 1) startingIndex = 0;
-  //   else startingIndex = startingIndex + 1;
-  //   console.log(startingIndex);
-  //   setCurrImg(props.images[startingIndex]);
-  // }
 
   return (
     <React.Fragment>
       <div>
         <img src={currImg} alt="no images" width="300" height="300" />
       </div>
-      <br></br>
+      <br />
       <button onClick={goBack}>Go Back</button>
-      <button onClick={goFoward}>Go Foward</button>
+      <button onClick={goForward}>Go Forward</button>
     </React.Fragment>
   );
 }
